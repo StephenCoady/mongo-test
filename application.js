dd23aca81c536dd0e5db70dc99dc97301017ff3b
Stephen Coady <scoady@redhat.com>
2017-10-01 13:52:10 +0100

    update

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 
@@ -25,12 +26,13 @@ var startTime = new Date();
 app.get('/', function(req, res) {
       var myCollection;
   console.log('hit the test endpoint')
-  var db = MongoClient.connect('mongodb://mongodb-1:27017', function(err, db) {
-    if (err)
-      throw err;
-    console.log("connected to the mongoDB !");
-    myCollection = db.collection('test_collection');
-  });
+  mongoose.connect('mongodb://mongodb-1.rhmap-3-node-mbaas:27017');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+      console.log("connected to the mongoDB !");
+      db.collection('test_collection');
+    });
   res.send('Application started: '+ startTime +' and is running for '+ (Date.now() / 1000 - runtimeTimer) +' seconds.<br><a href="./test">/test endpoint</a>');
 
 });
